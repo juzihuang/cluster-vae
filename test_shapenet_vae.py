dc3293437d60aa8f44d39b662ff7a287502c2de7
Yida Wang <yidawang.cn@gmail.com>
2017-05-15 09:36:47 +0800

    batch size to 64

--- test_shapenet_vae.py
+++ test_shapenet_vae.py
@@ -14,7 +14,7 @@ def test_shapenet():
     train_vae(
         files="./list_annotated_shapenet.csv",
         input_shape=[116, 116, 3],
-        batch_size=36,
+        batch_size=64,
         n_epochs=500,
         crop_shape=[112, 112, 3],
         crop_factor=1.0,
