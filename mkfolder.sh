47dcaf873a285925733b203c7ce9fa4f58ef9be1
Yida Wang <yidawang.cn@gmail.com>
2017-05-16 15:22:48 +0800

    shell for testing

--- mkfolder.sh
+++ mkfolder.sh
@@ -4,3 +4,37 @@ mkdir result_sita_clvae
 mkdir result_sita_vae
 mkdir result_shapenet_clvae
 mkdir result_shapenet_vae
+
+## Four tests on MNIST databse with variational/clustered without convolutional
+screen -r ae_mnist
+CUDA_VISIBLE_DEVICES=0 python3 test_mnist.py -o result_mnist_ae
+screen -d
+
+screen -r vae_mnist
+CUDA_VISIBLE_DEVICES=1 python3 test_mnist.py -v -o result_mnist_vae
+screen -d
+
+screen -r clae_mnist
+CUDA_VISIBLE_DEVICES=2 python3 test_mnist.py -k  -o result_mnist_clae
+screen -d
+
+screen -r clvae_mnist
+CUDA_VISIBLE_DEVICES=3 python3 test_mnist.py -v -k -o result_mnist_clvae
+screen -d
+
+## Four tests on ShapeNet databse with variational/clustered with convolutional
+screen -r ae_shapenet
+CUDA_VISIBLE_DEVICES=0 python3 test_shapenet.py -c -o result_shapenet_ae
+screen -d
+
+screen -r vae_shapenet
+CUDA_VISIBLE_DEVICES=1 python3 test_shapenet.py -c -v -o result_shapenet_vae
+screen -d
+
+screen -r clae_shapenet
+CUDA_VISIBLE_DEVICES=2 python3 test_shapenet.py -c -k -o result_shapenet_clae
+screen -d
+
+screen -r clvae_shapenet
+CUDA_VISIBLE_DEVICES=3 python3 test_shapenet.py -c -v -k -o result_shapenet_clvae
+screen -d
